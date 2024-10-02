4b96e9aac93c47d58fb921b93f6e8a6f59abd3d4
Nacho Serrano <ignaserrano@gmail.com>
2024-10-02 18:15:52 +0200

    Added substract to the calculator

--- src/Operators.java
+++ src/Operators.java
@@ -1,8 +1,12 @@
 public class Operators {
 
-public int add(int a, int b) {
-    return a + b;
-}
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 
 
 }
